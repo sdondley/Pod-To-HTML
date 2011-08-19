79d9ca8406192fe67f1c7f703b32d14c9f5efddd
Anthony Parsons <flussence+git@gmail.com>
2011-08-19 01:05:22 +0100

    Prettify Text::Escape, export functions directly
    Seems to be little point in having a wrapper for two functions, when it's longer
    than just writing their names out directly...

--- lib/Text/Escape.pm
+++ lib/Text/Escape.pm
@@ -1,41 +1,37 @@
 use v6;
 module Text::Escape;
 
-sub escape_html($str as Str) {
-    $str.subst('&', '&amp;', :g).subst('<', '&lt;', :g
-         ).subst('>', '&gt;', :g).subst('"', '&quot;', :g
-         ).subst("'", '&#39;', :g);
-}
-
 sub escape($str as Str, Str $how) is export {
     given $how.lc {
         when 'none'         { $str }
         when 'html'         { escape_html($str) }
-        when 'uri' | 'url'  { escape_str($str, &escape_uri_char)  }
+        when 'uri' | 'url'  { escape_uri($str)  }
         default { fail "Don't know how to escape format $how yet" }
     }
 }
 
-sub escape_uri_char(Str $c) returns Str {
-    return q{+} if $c eq q{ };
-
-    my $allowed = 'abcdefghijklmnopqrstuvwxyz'
-                ~ 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                ~ '0123456789'
-                ~ q{-_.!~*'()};
-
-    return $c if defined $allowed.index($c);
-
-    # TODO: each char should be UTF-8 encoded, then its bytes %-encoded
-    return q{%} ~ ord($c).fmt('%x');
+sub escape_html(Str $str) returns Str is export {
+    $str.subst(q{&}, q{&amp;}, :g)\
+        .subst(q{<}, q{&lt;}, :g)\
+        .subst(q{>}, q{&gt;}, :g)\
+        .subst(q{"}, q{&quot;}, :g)\
+        .subst(q{'}, q{&#39;}, :g);
 }
 
-sub escape_str(Str $str, Callable $callback) returns Str {
-    my $result = '';
-    for ^$str.chars -> $index {
-        $result ~= $callback($str.substr: $index, 1)
-    }
-    return $result;
+sub escape_uri(Str $str) returns Str is export {
+    my Str $allowed = 'abcdefghijklmnopqrstuvwxyz'
+                    ~ 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                    ~ '0123456789'
+                    ~ q{-_.!~*'()};
+
+    return [~] $str.comb.map(-> $char {
+        given $char {
+            when q{ }                           { q{+} }
+            when defined $allowed.index($char)  { $char }
+            # TODO: each char should be UTF-8 encoded, then its bytes %-encoded
+            default                             { q{%} ~ ord($char).fmt('%x') }
+        }
+    });
 }
 
 # vim:ft=perl6
